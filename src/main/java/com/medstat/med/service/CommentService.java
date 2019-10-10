090af32754f686d4fc6f6aa773422f2b2c07cc73
Jan <jabajaryn@gmail.com>
2019-10-10 10:19:48 +0200

    comment tested

---
+++ src/main/java/com/medstat/med/service/CommentService.java
@@ -0,0 +1,28 @@
+package com.medstat.med.service;
+
+import com.medstat.med.domain.Comment;
+import com.medstat.med.domain.Note;
+import com.medstat.med.domain.User;
+import com.medstat.med.repos.CommentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    @Autowired
+    CommentRepo commentRepo;
+
+    public boolean addComment(Note note, User author, String text) {
+        try {
+            Comment comment = new Comment();
+            comment.setAuthor(author);
+            comment.setText(text);
+            comment.setNote(note);
+
+            commentRepo.save(comment);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/com/medstat/med/service/CommentServiceTest.java
@@ -0,0 +1,81 @@
+package com.medstat.med.service;
+
+import com.medstat.med.domain.Note;
+import com.medstat.med.domain.User;
+import com.medstat.med.repos.CommentRepo;
+import com.medstat.med.repos.NoteRepo;
+import com.medstat.med.repos.UserRepo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class CommentServiceTest {
+
+    private static final String USERNAME_TEST = "username1";
+    private static final String USER_PASSWORD_TEST = "password1";
+    private static final String NOTE_COMMENT_TEST = "this is my comment";
+    private static final String COMMENT_TEXT_TEST = "this is very bad post.";
+    
+    @Autowired
+    NoteService noteService;
+
+    @Autowired
+    NoteRepo noteRepo;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepo userRepo;
+
+    @Autowired
+    CommentService commentService;
+    @Autowired
+    CommentRepo commentRepo;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addComment() {
+        //add user
+
+        userService.addUser(USERNAME_TEST, USER_PASSWORD_TEST);
+        User user = userRepo.findByUsername(USERNAME_TEST);
+
+        // add note
+        Note note = new Note();
+        note.setComment(NOTE_COMMENT_TEST);
+        noteRepo.save(note);
+
+
+        //add comment
+        boolean b = commentService.addComment(note, user, COMMENT_TEXT_TEST);
+        assertTrue(b);
+
+
+        //delete comment
+
+        commentRepo.deleteByAuthor_Id(user.getId());
+
+        //delete user
+        userRepo.deleteByUsername(USERNAME_TEST);
+        //delete note
+        noteRepo.deleteByComment(note.getComment());
+
+    }
+}
