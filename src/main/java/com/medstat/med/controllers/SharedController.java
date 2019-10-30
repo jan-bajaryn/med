ae0dbe054c308a979c5bea678d6fe015aa4e4281
Jan <jabajaryn@gmail.com>
2019-10-30 16:10:19 +0100

    added search by disease

--- src/main/java/com/medstat/med/controllers/SharedController.java
+++ src/main/java/com/medstat/med/controllers/SharedController.java
@@ -1,11 +1,9 @@
 package com.medstat.med.controllers;
 
-import com.medstat.med.domain.Comment;
-import com.medstat.med.domain.Note;
-import com.medstat.med.domain.Role;
-import com.medstat.med.domain.User;
+import com.medstat.med.domain.*;
 import com.medstat.med.repos.NoteRepo;
 import com.medstat.med.repos.UserRepo;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,8 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -47,15 +48,15 @@ public class SharedController {
             note.getComments().sort(Comparator.comparing(Comment::getId));
             model.addAttribute("note", note);
             //working with user
-            if (user!=null){
+            if (user != null) {
                 Optional<User> byIdUser = userRepo.findById(user.getId());
-                if (byIdUser.isPresent()){
+                if (byIdUser.isPresent()) {
                     user = byIdUser.get();
 
                     if (user.getRoles().contains(Role.ADMIN))
-                        model.addAttribute("role","admin");
+                        model.addAttribute("role", "admin");
                     else if (user.getRoles().contains(Role.EDITOR))
-                        model.addAttribute("role","editor");
+                        model.addAttribute("role", "editor");
 
                 }
             }
@@ -69,6 +70,30 @@ public class SharedController {
     @GetMapping("/search")
     public String search(Model model) {
         model.addAttribute("notes", noteRepo.findAll());
+        return "shared/search";
+    }
+
+    @GetMapping("/filter")
+    public String filter(Model model,
+                         @RequestParam(required = false, name = "filter") String filter) {
+        if (filter == null || filter.isEmpty())
+            return "redirect:/shared/search";
+
+        List<Note> notes = noteRepo.findAll().stream()
+                .filter(note -> {
+                    List<String> noteNames = note.getDiseases().stream()
+                            .map(Disease::getName)
+                            .collect(Collectors.toList());
+                    for (String name : noteNames) {
+                        if (name.contains(filter)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                })
+                .collect(Collectors.toList());
+
+        model.addAttribute("notes", notes);
         return "shared/search";
     }
 
